05d4d6eda4d332d305848eb64851b70aba2b6a25
David <David@DESKTOP-TGIS9QJ>
2025-01-16 15:15:28 +0100

    viddakovic

---
+++ Tests/Services/AutentifikacioniServisi/AutentifikacioniServisiTests.cs
@@ -0,0 +1,67 @@
+﻿using Domain.Modeli;
+using Domain.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Services.AutentifikacioniServisi
+{
+    public class AutentifikacioniServisiTests
+    {
+        // Servis koji se lazira
+        Mock<IServisAutentifikacije> _authServis;
+
+        // Konstruktor inicijalizator
+        public AutentifikacioniServisiTests() => _authServis = new Mock<IServisAutentifikacije>();
+
+        [SetUp]
+        public void Setup()
+        {
+            // Metoda koja priprema podatke pre pokretanja testova
+            _authServis = new Mock<IServisAutentifikacije>();
+        }
+
+        [Test]
+        [TestCase("maja", "123")]
+        public void PrijavaSaIspravnimPodacima_VracaTrue(string korisnickoIme, string lozinka)
+        {
+            // Kreiranje potrebnih promenljivih za test
+            var korisnik = new Korisnik(korisnickoIme, lozinka);
+
+            // Priprema Moq-a, koja je ocekivana vrednost koja treba da se vrati
+            _authServis.Setup(x => x.Prijava(korisnickoIme, lozinka)).Returns((true, korisnik));
+
+            // Simuliranje ponasanja nad laziranim objektom servisa
+
+
+            // Provera da li je ocekivani rezultat jednak rezultatu testa
+            Assert.That(uspesnaAutentifikacija, Is.True);
+            Assert.That(prijavljen, Is.Not.Null);
+            Assert.That(prijavljen.KorisnickoIme, Is.EqualTo(korisnickoIme));
+            Assert.That(prijavljen.Lozinka, Is.EqualTo(lozinka));
+        }
+
+        [Test]
+        [TestCase("danijel", "123")]
+        [TestCase("jovana", "123")]
+        public void PrijavaSaNeispravnimPodacima_VracaFalse(string korisnickoIme, string lozinka)
+        {
+            // Kreiranje potrebnih promenljivih za test
+            Korisnik korisnik;
+
+            // Priprema Moq-a, koja je ocekivana vrednost koja treba da se vrati
+            _authServis.Setup(x => x.Prijava(korisnickoIme, lozinka)).Returns((false, new Korisnik()));
+
+            // Simuliranje ponasanja nad laziranim objektom servisa
+            (bool uspesnaAutentifikacija, korisnik) = _authServis.Object.Prijava(korisnickoIme, lozinka);
+
+            // Provera da li je ocekivani rezultat jednak rezultatu testa
+            Assert.That(uspesnaAutentifikacija, Is.False);
+            Assert.That(korisnik.KorisnickoIme, Is.Empty);
+        }
+    }
+}
